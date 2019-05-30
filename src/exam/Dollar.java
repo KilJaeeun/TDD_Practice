7ad9555d4dbc2cf0e16adc0bc387c5246f296e13
changjun89 <leechang0423@naver.com>
2019-05-31 00:25:21 +0900

    * 금액에 어떤수를 곱한 금액을 결과로 얻는다

---
+++ src/exam/Dollar.java
@@ -0,0 +1,15 @@
+package exam;
+
+public class Dollar {
+
+  public int amount;
+
+  public Dollar(int amount) {
+    this.amount = amount;
+
+  }
+
+  public void times(int multiplier) {
+    amount *= multiplier;
+  }
+}
---
+++ test/exam/DollarTest.java
@@ -0,0 +1,17 @@
+package exam;
+
+import static junit.framework.TestCase.assertEquals;
+
+import org.junit.Test;
+
+public class DollarTest {
+
+  @Test
+  public void testMultiplication() {
+    Dollar five = new Dollar(5);
+    five.times(2);
+    assertEquals(10, five.amount);
+
+  }
+
+}

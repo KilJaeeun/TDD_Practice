247968e7698d4dacf6ebd9bb03254dcbd51aa0d1
changjun89 <leechang0423@naver.com>
2019-06-04 22:18:09 +0900

    * 5CHF X 2 = 10CHF 완료

--- test/exam/DollarTest.java
+++ test/exam/DollarTest.java
@@ -13,7 +13,6 @@ public class DollarTest {
     Dollar five = new Dollar(5);
     assertEquals(new Dollar(10), five.times(2));
     assertEquals(new Dollar(15), five.times(3));
-
   }
 
   @Test
@@ -23,6 +22,13 @@ public class DollarTest {
     assertFalse(
         new Dollar(5).equals(new Dollar(6)));
 
+  }
+
+  @Test
+  public void testFrancMultiplication() {
+    Franc five = new Franc(5);
+    assertEquals(new Franc(10), five.times(2));
+    assertEquals(new Franc(15), five.times(3));
   }
 
 }
